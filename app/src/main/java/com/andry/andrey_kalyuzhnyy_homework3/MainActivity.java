ab115dbdd23d30b6f490181e3f6f6ccf38304c59
Andriy Kalyuzhnyy <andrydonalts@gmail.com>
2016-05-03 12:39:36 +0300

    ver 5 (with DragAndDrop)

--- app/src/main/java/com/andry/andrey_kalyuzhnyy_homework3/MainActivity.java
+++ app/src/main/java/com/andry/andrey_kalyuzhnyy_homework3/MainActivity.java
@@ -1,11 +1,14 @@
 package com.andry.andrey_kalyuzhnyy_homework3;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayout;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private View dialerButton;
     private Button appsButton;
     private View messageButton;
+    private GridLayout gridLayout;
     private AppsManager appsManager;
     private ArrayList<AppsDetail> mainScreenApps;
 
@@ -80,9 +84,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    class LongPressListener implements View.OnLongClickListener{
+
+        @Override
+        public boolean onLongClick(View view) {
+            final ClipData data = ClipData.newPlainText("", "");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+            view.startDrag(data, shadowBuilder, view, 0);
+            view.setVisibility(View.INVISIBLE);
+            return true;
+        }
+    }
+
     // putting views in gridlayout dynamically
     public void setGridLayout() {
-        GridLayout gridLayout = (GridLayout) findViewById(R.id.activity_main_gridLayout);
+        gridLayout = (GridLayout) findViewById(R.id.activity_main_gridLayout);
+        gridLayout.setOnDragListener(new DragListener());
+
         LayoutInflater layoutInflater = (LayoutInflater)
                 this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
@@ -91,22 +109,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             gridLayout.setRowCount(3);
         }
 
-        int row = -1;
         for (int i = 0; i < 15; i++) {
 
             final AppsDetail appsDetail = mainScreenApps.get(i);
             View appItemView = layoutInflater.inflate(R.layout.app_item, null, false);
 
-            // getting row and column number, because otherwise sequence views is wrong
-            int col = 0;
-            if (isPortraitOrientation() == false) {
-                col = i % 5;
-            } else {
-                col = i % 3;
-            }
-            if (col == 0){
-                row++;
-            }
 
             ImageView icon = (ImageView) appItemView.findViewById(R.id.app_item_icon);
             TextView label = (TextView) appItemView.findViewById(R.id.app_item_label);
@@ -119,17 +126,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     appsManager.startApp(appsDetail);
                 }
             });
+            appItemView.setOnLongClickListener(new LongPressListener());
 
             GridLayout.LayoutParams params = new GridLayout.LayoutParams();
             params.width = 0;
             params.height = 0;
-            params.columnSpec = GridLayout.spec(col, 1f);
-            params.rowSpec = GridLayout.spec(row, 1f);
+            params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
+            params.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
             params.setMargins(3, 3, 3, 3);
             appItemView.setLayoutParams(params);
-            gridLayout.addView(appItemView, 0);
+            gridLayout.addView(appItemView);
 
         }
+    }
+
+    class DragListener implements View.OnDragListener{
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            View view = (View) event.getLocalState();
+            switch (event.getAction()){
+                case DragEvent.ACTION_DRAG_LOCATION :
+                    if (view == v) {
+                        return true;
+                    }
+
+                    int index = calculateNewIndex(event.getX(), event.getY());
+
+                    gridLayout.removeView(view);
+                    gridLayout.addView(view, index);
+                    break;
+                case DragEvent.ACTION_DROP :
+                    view.setVisibility(View.VISIBLE);
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED :
+                    if (!event.getResult())
+                        view.setVisibility(View.VISIBLE);
+            }
+            return true;
+        }
+    }
+
+
+    private int calculateNewIndex(float x, float y) {
+
+        float cellWidth = gridLayout.getWidth() / gridLayout.getColumnCount();
+        int column = (int) (x /cellWidth);
+
+        float cellHeight = gridLayout.getHeight() / gridLayout.getRowCount();
+        int row = (int) Math.floor(y / cellHeight);
+
+        int index = row * gridLayout.getColumnCount() + column;
+        if (index >= gridLayout.getChildCount())
+            index = gridLayout.getChildCount() - 1;
+
+        Log.d("calcNewInd", "row = " + Integer.toString(row) + " col = " + Integer.toString(column) + " index = " + Integer.toString(index));
+        return index;
     }
 
     private boolean isPortraitOrientation(){
